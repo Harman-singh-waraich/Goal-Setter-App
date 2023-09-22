a7a17313bc7a79d5536f4439c7b4625bbba37087
Harman-Singh-Waraich <WaraichHarman68@gmail.com>
2023-09-22 21:02:46 +0530

    goal setter app

---
+++ functions/controllers/goalController.js
@@ -0,0 +1,89 @@
+const asyncHandler = require('express-async-handler')
+
+const Goal = require("../modal/goalModal")
+const User = require("../modal/userModal")
+
+// @desc Get goals
+// @route GET /api/goals
+// @access Private
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find({user: req.user.id})
+  res.status(200).json(goals);
+});
+
+// @desc Set goals
+// @route POST /api/goals
+// @access Private
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+  const goal = await Goal.create({
+    text: req.body.text,
+    user: req.user.id,
+  })
+  res.status(200).json(goal);
+});
+
+// @desc Update goal
+// @route PUT /api/goals/:id
+// @access Private
+const updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+
+  if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+  
+  const user = await User.findById(req.user.id)
+  // check for user
+  if(!user){
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //check right user is updating
+  if(goal.user.toString() !== user.id){
+    res.status(401)
+    throw new Error('Not autorised')
+  }
+  
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{new: true})
+  res.status(200).json(updatedGoal);
+});
+
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @access Private
+const deleteGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id)
+  if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+  }
+  const user = await User.findById(req.user.id)
+  // check for user
+  if(!user){
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  //check right user is updating
+  if(goal.user.toString() !== user.id){
+    res.status(401)
+    throw new Error('Not autorised')
+  }
+  
+  const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
+  console.log(deletedGoal)
+  res.status(200).json(deletedGoal);
+});
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
---
+++ functions/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const {errorHandler} = require("./middleware/errorMiddleware")
+const connectDB = require('./config/db');
+const serverless = require("serverless-http");
+const port = process.env.PORT || 5000;
+
+connectDB()
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+app.use("/api/server/goals", require("./routes/goalRoutes"));
+app.use("/api/server/users", require("./routes/userRoutes"));
+
+app.use(errorHandler)
+module.exports.handler = serverless(app)
+// app.listen(port, () => console.log(`Server started on port ${port}`));
